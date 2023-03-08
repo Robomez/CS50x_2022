e490f899e1793791a70893192340936d1f6792a9
Robomez <alextimofee@gmail.com>
2023-03-08 19:46:55 +0300

    Delete everything

--- CS50Example5_Notes-Room,SQLite/app/src/main/java/com/example/cs50example5notes/NotesDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.example.cs50example5notes;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {Note.class}, version = 1)
-public abstract class NotesDatabase extends RoomDatabase {
-    public abstract NoteDao noteDao();
-}
